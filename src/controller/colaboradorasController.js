463e7d09e85083c175894e237aaaeed8e3875d59
Tereza25 <rh.cris.ead@gmail.com>
2021-06-17 01:02:03 -0300

    Atualizando atividade feita na aula

--- src/controller/colaboradorasController.js
+++ src/controller/colaboradorasController.js
@@ -1,6 +1,7 @@
-const colaboradoras = require("../models/colaboradoras")
-const SECRET = process.env.SECRET
+const colaboradoras = require("../models/colaboradoras");
+const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
+const SECRET = process.env.SECRET
 
 
 const getAll = (req, res) => {
@@ -12,32 +13,54 @@ const getAll = (req, res) => {
     return res.status(401).send('erro no header');
   }
 
-/*jwt.verify(token, SECRET, function(erro) {
+jwt.verify(token, SECRET, function(erro) {
     if (erro) {
       return res.status(403).send('Não autorizado');
-    }*/
+    }
 
-        colaboradoras.find(function (err, colaboradoras){
+       colaboradoras.find(function (err, colaboradoras){
       res.status(200).send(colaboradoras)
     }) 
-//  })    
+  })    
 };
 
 const postColaboradora = (req, res) => {
-  console.log(req.body);
+  const senhaComHash = bcrypt.hashSync(req.body.password, 10);
+  req.body.password = senhaComHash;
+  const colaboradora = new colaboradoras(req.body);
 
-  let colaboradora = new colaboradoras(req.body);
-    colaboradora.save(function(err){
-    if (err) res.status(500).send({ message: err.message })
+  colaboradora.save(function(err) {
+    if (err) {
+      res.status(500).send({ message: err.message })
+    }
 
-    res.status(201).send(colaboradora.toJSON());
+    res.status(201).send(colaboradora.toJSON())
   })
 };
 
+const login = (req, res) => {
+  colaboradoras.findOne({ email: req.body.email }, function(error, colaboradora) {
+    if (!colaboradora) {
+	   return res.status(404).send(`Não existe colaboradora com o email ${req.body.email}`);
+    }
+
+    const senhaValida = bcrypt.compareSync(req.body.password, colaboradora.password);
+    
+    if (!senhaValida) {
+      return res.status(403).send(`que senha é essa hein`);
+    }
+
+    const token = jwt.sign({ email: req.body.email }, SECRET);
+
+      return res.status(200).send(token);
+  });
+}
+    
 
 
 
 module.exports = {
     getAll,
     postColaboradora,
+    login,
 }
