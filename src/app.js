5c1126436d1c9a3fad1e1a084b07b35a4192d0f0
Tereza25 <rh.cris.ead@gmail.com>
2021-06-17 17:04:03 -0300

    atualizando repositório

---
+++ src/app.js
@@ -0,0 +1,17 @@
+const express = require("express")
+const app = express()
+
+app.use(express.json())
+
+app.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*") // informo que minha api poderá ser chamada de qualquer lugar. Por um browser, por exemplo.
+    res.header(
+        "Access-Control-Allow-Headers",
+        "Origin, X-Requested-With, Content-Type, Accept"
+    )
+    next()
+    // como criei uma função dentro do app.use, preciso dar um "next()" para mandar ele seguir para a próxima middleware. 
+    // se eu não faço isso, a requisição vai ficar travada aí.
+})
+
+module.exports = app
