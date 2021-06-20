b489399e6a9f93ca5ef6bddd0369d6acee6e1c5e
amandatrololo <amandablopess@gmail.com>
2021-06-19 21:24:24 -0300

    Semana 15, tive dificuldades com a senha, pesquisarei mais sobre o assunto

--- src/app.js
+++ src/app.js
@@ -1,7 +1,13 @@
+require('dotenv-safe').config();
 const express = require("express")
 const app = express()
 
 app.use(express.json())
+
+mongoose.connect(process.env.MONGODB_URL,  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+});
 
 app.use(function (req, res, next) {
     res.header("Access-Control-Allow-Origin", "*") // informo que minha api poderá ser chamada de qualquer lugar. Por um browser, por exemplo.
@@ -13,5 +19,10 @@ app.use(function (req, res, next) {
     // como criei uma função dentro do app.use, preciso dar um "next()" para mandar ele seguir para a próxima middleware. 
     // se eu não faço isso, a requisição vai ficar travada aí.
 })
+
+module.exports = app
+
+app.use("/", index)
+app.use("/colaboradoras", colaboradoras)
 
 module.exports = app
