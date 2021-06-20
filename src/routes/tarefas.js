88c382d5bc0237cf0148de150a174cfbb31e6ce7
Anaísa Mayara <anaisateodoro@hotmail.com>
2021-06-19 23:01:58 -0300

    Exercício da semana 15

---
+++ src/routes/tarefas.js
@@ -0,0 +1,15 @@
+require('dotenv-safe').config();
+
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/tarefasController");
+
+router.get("/", controller.getAll);
+router.get("/:id", controller.getById);
+router.post("/", controller.postTask);
+router.put("/:id", controller.putTask);
+router.delete("/:id", controller.deleteTask);
+
+
+
+module.exports = router;
