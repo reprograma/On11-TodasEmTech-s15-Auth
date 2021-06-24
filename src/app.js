568c89606fc657854e3eb1ef35a21d5ca4e5b766
littlejanne <54994679+littlejanne@users.noreply.github.com>
2021-06-24 00:29:17 -0300

    Trabalho feito com atraso.

--- src/app.js
+++ src/app.js
@@ -1,17 +1,49 @@
 const express = require("express")
+const bodyParser = require("body-parser")
+const mongoose = require("mongoose")
+require('dotenv-safe').config();
+
 const app = express()
 
-app.use(express.json())
+//String de conexão
+mongoose.connect(process.env.MONGO_URL,  {
+     useNewUrlParser: true,
+     useUnifiedTopology: true
+});
+
+//Conexão com o mongo
+let db = mongoose.connection;
+
+// Captura de erro ou sucesso na conexão
+db.on("error", console.log.bind(console, "connection error:"))
+db.once("open", function (){
+  console.log("conexão feita com sucesso.")
+})
+
+
+//rotas
+const index = require("./routes/index")
+const colaboradoras = require("./routes/colaboradoras")
+const tarefas = require('./routes/tarefas')
+
+//configurar body parser
+app.use(bodyParser.json())
+// app.use(express.json()); - Podemos usar a propria função de parser de json do express, sem a necessidade de instalar o body parser
+
 
 app.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*") // informo que minha api poderá ser chamada de qualquer lugar. Por um browser, por exemplo.
+    res.header("Access-Control-Allow-Origin", "*") 
     res.header(
         "Access-Control-Allow-Headers",
         "Origin, X-Requested-With, Content-Type, Accept"
-    )
-    next()
-    // como criei uma função dentro do app.use, preciso dar um "next()" para mandar ele seguir para a próxima middleware. 
-    // se eu não faço isso, a requisição vai ficar travada aí.
-})
+        )
+        next()
+   
+    })
+
+app.use("/", index)
+app.use("/colaboradoras", colaboradoras)
+app.use("/tarefas", tarefas)
+
 
 module.exports = app
---
+++ src/routes/colaboradoras.js
@@ -0,0 +1,8 @@
+const express = require("express")
+const router = express.Router()
+const controller = require("../controller/colaboradorasController")
+
+router.get("/", controller.authVerify, controller.getAll)
+router.post("/", controller.postColaboradora)
+router.post("/login", controller.login, controller.createToken)
+module.exports = router;
