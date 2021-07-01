227a6190798c3bad8cad00d58ed31009a5e003be
deisiane16 <deisimiranda48@gmail.com>
2021-06-30 23:21:24 -0300

    exercicio semana 15

---
+++ src/models/colaboradoras.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose')
+
+const colaboradora = new mongoose.colaboradora({
+    nome: mongoose.colaboradora.Types.ObjectId,
+    nome: {
+        type: String,
+        required: true
+
+    }
+})
+
+module.exports = mongoose.model('estudio', estudioSchema )
